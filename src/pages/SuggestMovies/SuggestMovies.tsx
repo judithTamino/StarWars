e25538e82bc3e2192751e150593e2f4b787e1255
יהודית טמינו <taminojudit@hotmail.co.il>
2025-05-12 14:18:48 +0300

    display movies by number of favorite characters

--- src/pages/SuggestMovies/SuggestMovies.tsx
+++ src/pages/SuggestMovies/SuggestMovies.tsx
@@ -1,60 +1,78 @@
-import { Box, Card, Grid } from '@mui/material';
+import { Box, Card, Chip, Grid, Typography } from '@mui/material';
 import { FunctionComponent, useEffect, useState } from 'react';
 import { useFavoriteCharacters } from '../../context/favoriteCharacterContext';
 import { IFilms } from '../../interfaces/IFilms';
 import { getFilm } from '../../services/filmsService';
 import { errMsg } from '../../services/feedbackService';
+import { getCharacterId } from '../../utils/getCharacterId';
 
 interface SuggestMoviesProps {}
 
 const SuggestMovies: FunctionComponent<SuggestMoviesProps> = () => {
-  const [films,] = useState<IFilms[]>([]);
+  const [films, setFilms] = useState<IFilms[]>([]);
+  const [filmScore, setFilmScore] = useState<Record<string, number>>({});
   const { favoriteCharacters } = useFavoriteCharacters();
-  const filmScore: Record<string, number> = {};
-  let sortedFilms:string[] = [];
+
+  const countCharacters = () => {
+    const newScore: Record<string, number> = {};
+
+    favoriteCharacters.forEach((character) => {
+      character.films.forEach((url) => {
+        newScore[url] = (newScore[url] || 0) + 1;
+      });
+    });
+
+    setFilmScore(newScore);
+  };
 
   useEffect(() => {
     countCharacters();
+  }, [favoriteCharacters]);
 
-sortedFilms.forEach((film) => {
-  getFilm(film)
-  .then((response) => {
-    films.push(response.data);
-  })
-  .catch((error) => errMsg(error.response.data))
-})
-  }, []);
+  useEffect(() => {
+    const sortedFilmUrls = Object.entries(filmScore)
+      .sort((a, b) => b[1] - a[1])
+      .map(([url]) => url);
 
-  // func that count how many characters appear in each film
-  function countCharacters() {
-    favoriteCharacters.forEach((character) => {
-      character.films.forEach((url) => {
-        filmScore[url] = (filmScore[url] || 0) + 1;
-      });
+    sortedFilmUrls.forEach((film) => {
+      getFilm(film)
+        .then((response) => {
+          setFilms(prev => [...prev, response.data])
+        })
+        .catch((error) => errMsg(error.response.data));
     });
-    sortFilms();
-  }
+  }, [filmScore]);
 
-  // func => sort the films based on the number of fav characters
-  function sortFilms() {
-   sortedFilms =  Object.keys(filmScore).sort((filmOne, filmTwo) => filmScore[filmTwo] - filmScore[filmOne]).map((url => url));
-  }
-
-  return <Box>
-<Grid container spacing={4}>
-  {
-    films.map((film, index) => (
-      <Grid key={index} size={{xs: 12, sm: 6, md: 4}}>
-        <Card sx={{
-          maxWidth: 345,
-          height: 400,
-
-        }}></Card>
-      </Grid>
-    ))
-  }
-</Grid>
-  </Box>;
+ 
+  return (
+    <Box>
+      {films.length === 0 ? (
+        <Typography component='h2' variant='h1'>
+          No Suggest Movies Found
+        </Typography>
+      ) : (
+        <Grid container spacing={4}>
+          {films.map((film, index) => (
+            <Grid key={index} size={{ xs: 12, sm: 6, md: 4 }}>
+              <Card
+                sx={{
+                  maxWidth: 345,
+                  height: 500,
+                  backgroundImage: `url(./films/${getCharacterId(
+                    film.url
+                  )}.jpg)`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                }}
+              >
+                <Chip sx={{ m: 2 }} label={`${index + 1}`} color='primary' />
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Box>
+  );
 };
 
 export default SuggestMovies;
